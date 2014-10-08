ff987303822abe3ea6b8969dd0aaa5de71de5d44
jyzackoh <zackkohjunyang@gmail.com>
2014-10-09 01:45:10 +0800

    Modified front controller to support a starting page, and renamed methods to be more meaningful.

--- splash/splash/urls.py
+++ splash/splash/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from splash.views import new_page, load_page, not_found_page
+from splash.views import index, new_program, load_program, not_found_page
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,8 +8,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'splash.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
-    url(r'^$', new_page),
-    url(r'^([0-9a-zA-Z]{5})/$', load_page),
+    url(r'^$', index),
+    url(r'^new_program$', new_program),
+    url(r'^([0-9a-zA-Z]{5})/$', load_program),
     url(r'^([0-9a-zA-Z]{0,9999})/$', not_found_page),
     url(r'^admin/', include(admin.site.urls)),
 )
