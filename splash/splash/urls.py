515ae69cfc828719cf3d6b1255d357600fbda3dd
jyzackoh <zackkohjunyang@gmail.com>
2014-10-09 02:16:36 +0800

    Updated urls.py and views.py to support creating private programs.

--- splash/splash/urls.py
+++ splash/splash/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     
     url(r'^$', index),
-    url(r'^new_program$', new_program),
+    url(r'^new_program/([A-Z]{2})/$', new_program),
     url(r'^([0-9a-zA-Z]{5})/$', load_program),
     url(r'^([0-9a-zA-Z]{0,9999})/$', not_found_page),
     url(r'^admin/', include(admin.site.urls)),
--- splash/splash/views.py
+++ splash/splash/views.py
@@ -5,10 +5,12 @@ import random, string
 def index(request):
 	return render(request, 'index.html', {})
 
-def new_program(request):
+def new_program(request, permission):
+	if (permission != 'PU' and permission != 'PR'):
+		return redirect('/')
 	generate_page_args = {
 		'program_code': generate_program_code(),
-		'permission': 'PR',
+		'permission': permission,
 		'owner': ''
 	}
 	return generate_new_page(request, generate_page_args)
@@ -19,7 +21,7 @@ def load_program(request, program_code):
 	if (gateway.program_is_exist(program_code)):
 		if (gateway.program_is_private(program_code)):
 			#Check if authorized and program owner == user. if yes, return page. else, redirect to main page with unauthorized msg
-			return render(request, 'program.html', {"url_sub_folder": program_code, "request": request})
+			return render(request, 'program.html', {"url_sub_folder": program_code + 'is private', "request": request})
 		else:
 			return render(request, 'program.html', {"url_sub_folder": program_code})
 	else:
