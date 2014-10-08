08b3f87cc3b99b0821ed91b108d589ac4ad1a550
jyzackoh <zackkohjunyang@gmail.com>
2014-10-09 01:43:34 +0800

    Implemented models.py for the schema and gateway.py as the gateway to query the database

---
+++ splash/splash/gateway.py
@@ -0,0 +1,28 @@
+from splash.models import Programs
+
+def add_new_program(program_code, permission, owner):
+	Programs.objects.create(program_code=program_code, serialized_program="", permission=permission, owner=owner)
+	pass
+
+def update_program(program_code, serialized_program):
+	pass
+
+def is_exist(program_code):
+	program_obj = None
+	try:
+		program_obj = Programs.objects.get(program_code=program_code)
+	except Programs.DoesNotExist:
+		program_obj = None
+
+	if (program_obj == None):
+		return False
+	else:
+		return True
+
+def is_private(program_code):
+	if (is_exist(program_code)):
+		program_obj = Programs.objects.get(program_code=program_code)
+		if (program_obj.permission == 'PR'):
+			return True
+	else:
+		return False
---
+++ splash/splash/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Programs(models.Model):
+	PERMISSIONS = (
+		('PU', 'public'),
+		('PR', 'private'),
+	)
+	program_code = models.CharField(max_length=5, primary_key=True)
+	serialized_program = models.TextField()
+	permission = models.CharField(max_length=2, choices=PERMISSIONS, default='PU')
+	owner = models.CharField(max_length=255)
