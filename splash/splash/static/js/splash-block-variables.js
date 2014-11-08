812ebca8d9bbf068f91b1b36027ecc2ed48d6d86
jyzackoh <zackkohjunyang@gmail.com>
2014-11-08 17:06:52 +0800

    Added some definitions for pre-defined variables

--- splash/splash/static/js/splash-block-variables.js
+++ splash/splash/static/js/splash-block-variables.js
@@ -20,13 +20,85 @@ splash.VariableBlock.prototype.render = function(){
   return htmlElement;
 }
 
-splash.SpritePositionBlock = function SpritePositionBlock(parameters){
+splash.SpriteXPositionBlock = function SpriteXPositionBlock(parameters){
 	splash.VariableBlock.call(this);
 	splash.Util.parseParameters(this, parameters);
 }
-splash.Util.inherits(splash.SpritePositionBlock, splash.VariableBlock);
-splash.SpritePositionBlock.prototype.name = "Sprite Position";
-splash.SpritePositionBlock.prototype.colour = "default";
-splash.SpritePositionBlock.prototype.codeSnippet = function(){
+splash.Util.inherits(splash.SpriteXPositionBlock, splash.VariableBlock);
+splash.SpriteXPositionBlock.prototype.name = "Sprite X Position";
+splash.SpriteXPositionBlock.prototype.colour = "default";
+splash.SpriteXPositionBlock.prototype.codeSnippet = function(){
+	return splash.SpriteManager.getCurrentSprite().getPosition().x;
+};
+
+
+splash.SpriteYPositionBlock = function SpriteYPositionBlock(parameters){
+	splash.VariableBlock.call(this);
+	splash.Util.parseParameters(this, parameters);
+}
+splash.Util.inherits(splash.SpriteYPositionBlock, splash.VariableBlock);
+splash.SpriteYPositionBlock.prototype.name = "Sprite Y Position";
+splash.SpriteYPositionBlock.prototype.colour = "default";
+splash.SpriteYPositionBlock.prototype.codeSnippet = function(){
+	return splash.SpriteManager.getCurrentSprite().getPosition().y;
+};
+
+
+splash.StageTopBlock = function StageTopBlock(parameters){
+	splash.VariableBlock.call(this);
+	splash.Util.parseParameters(this, parameters);
+}
+splash.Util.inherits(splash.StageTopBlock, splash.VariableBlock);
+splash.StageTopBlock.prototype.name = "Stage Top Position";
+splash.StageTopBlock.prototype.colour = "default";
+splash.StageTopBlock.prototype.codeSnippet = function(){
 	return splash.SpriteManager.getCurrentSprite().getPosition();
 };
+
+
+splash.StageBottomBlock = function StageBottomBlock(parameters){
+	splash.VariableBlock.call(this);
+	splash.Util.parseParameters(this, parameters);
+}
+splash.Util.inherits(splash.StageBottomBlock, splash.VariableBlock);
+splash.StageBottomBlock.prototype.name = "Sprite Position";
+splash.StageBottomBlock.prototype.colour = "default";
+splash.StageBottomBlock.prototype.codeSnippet = function(){
+	return splash.SpriteManager.getCurrentSprite().getPosition();
+};
+
+
+splash.StageLeftBlock = function StageLeftBlock(parameters){
+	splash.VariableBlock.call(this);
+	splash.Util.parseParameters(this, parameters);
+}
+splash.Util.inherits(splash.StageLeftBlock, splash.VariableBlock);
+splash.StageLeftBlock.prototype.name = "Sprite Position";
+splash.StageLeftBlock.prototype.colour = "default";
+splash.StageLeftBlock.prototype.codeSnippet = function(){
+	return splash.SpriteManager.getCurrentSprite().getPosition();
+};
+
+
+splash.StageRightBlock = function StageRightBlock(parameters){
+	splash.VariableBlock.call(this);
+	splash.Util.parseParameters(this, parameters);
+}
+splash.Util.inherits(splash.StageRightBlock, splash.VariableBlock);
+splash.StageRightBlock.prototype.name = "Sprite Position";
+splash.StageRightBlock.prototype.colour = "default";
+splash.StageRightBlock.prototype.codeSnippet = function(){
+	return splash.SpriteManager.getCurrentSprite().getPosition();
+};
+
+
+// splash.SpritePositionBlock = function SpritePositionBlock(parameters){
+// 	splash.VariableBlock.call(this);
+// 	splash.Util.parseParameters(this, parameters);
+// }
+// splash.Util.inherits(splash.SpritePositionBlock, splash.VariableBlock);
+// splash.SpritePositionBlock.prototype.name = "Sprite Position";
+// splash.SpritePositionBlock.prototype.colour = "default";
+// splash.SpritePositionBlock.prototype.codeSnippet = function(){
+// 	return splash.SpriteManager.getCurrentSprite().getPosition();
+// };
